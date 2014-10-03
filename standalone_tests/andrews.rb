898bb084f940b65ebbbdadbd581bf8d1f1ffe7c9
Nicolas Hammond <nhammond@hammondsoftware.com>
2014-10-03 00:48:58 -0400

    Add comments

--- standalone_tests/andrews.rb
+++ standalone_tests/andrews.rb
@@ -1,3 +1,8 @@
+# Standalone tests of Andrews numbers
+#
+# Run this from the directory above,
+# ruby standalone_tests/andrews.rb
+#
 require './lib/hand_record_utility.rb'
 
 @board = Hash.new
@@ -7,8 +12,10 @@ require './lib/hand_record_utility.rb'
 @board[:west]  = "SHDCAKQJT98765432"
 
 i = HandRecordUtility.to_andrews_number(@board)
+# 
 
+# This hand above should generate the number 1
 puts "i=#{i}"
-
+# 
 board = HandRecordUtility.andrews_number_to_board(i)
 HandRecordUtility.debug_board_short_form(board) 
