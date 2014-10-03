2658945c91c0f07408fddb14b3269ac83341b506
Nicolas Hammond <nhammond@hammondsoftware.com>
2014-10-03 00:49:27 -0400

    Add Andrews tests

--- spec/hand_record_utility_spec.rb
+++ spec/hand_record_utility_spec.rb
@@ -54,16 +54,35 @@ describe HandRecordUtility do
   end
 
   # Bounds test - failure
-  it "should fail with number 0 and an error message" do
+  it "should fail with number 0 and print an error message" do
     i = 0
     @board = HandRecordUtility.pavlicek_number_to_board(i)
     expect(@board).to eq(nil)
   end
 
-  it "should fail with number D+1 and an error message" do
+  it "should fail with number D+1 and print an error message" do
     i = HandRecordUtility::D + 1
     @board = HandRecordUtility.pavlicek_number_to_board(i)
     expect(@board).to eq(nil)
   end
 
+  # Bounds test - success
+  # Same hand record as Pavliceky
+  it "should test Andrews number of 1" do
+    i = 1
+    @board[:north] = "SAKQJT98765432"
+    @board[:east]  = "HAKQJT98765432"
+    @board[:south] = "DAKQJT98765432"
+    @board[:west]  = "CAKQJT98765432"
+    expect(HandRecordUtility.to_andrews_number(@board)).to eq(i)
+  end
+
+  ntimes = 10
+  it "should test 10 random Andrew numbers converting back and forth" do
+    (1..ntimes).each do |count|
+      i = rand(HandRecordUtility::D) + 1
+      @board = HandRecordUtility.andrews_number_to_board(i)
+     expect(HandRecordUtility.to_andrews_number(@board)).to eq(i)
+    end
+  end
 end
